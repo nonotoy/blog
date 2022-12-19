88d1e321edb88396e12db566a0966e3342c63cd9
nonotoy <56439819+nonotoy@users.noreply.github.com>
2022-12-19 21:27:55 +0900

    change owner

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,14 +3,14 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
+  title: 'Home',
   subtitle: 'Sweet & Powerful SvelteKit Blog Template',
   lang: 'en-US',
   description: 'Powered by SvelteKit/Urara',
   author: {
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    name: 'Nonotoy',
+    status: '🙃',
+    bio: 'Pet tomotuye uhaskopiwki p hemanta an?'
   },
   themeColor: '#3D4451'
 }
